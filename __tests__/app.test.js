ee7ff6d77935b72f6ccfdf32ec2e9bcb6f0cbad1
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-06-14 14:54:20 -0700

    more specific test expectations for all authors

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,8 +10,11 @@ describe('backend-express-template routes', () => {
 
   it('should return a list of authors', async () => {
     const res = await request(app).get('/authors');
+    const robin = res.body.find((author) => author.id === '1');
     expect(res.body.length).toEqual(7);
-    console.log('res.body', res.body);
+    expect(robin).toHaveProperty('name', 'Robin Wall Kimmerer');
+    expect(robin).toHaveProperty('dob', '01/01/1953');
+    expect(robin).toHaveProperty('pob', 'New York, NY');
   });
 
 
